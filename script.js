f9cf47b3cf1c6a2d031fb84301e8de2188b23e8c
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-10-18 15:56:26 +0530

    added backspace

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ Array.from(buttons).forEach((button)=>{
       string = ""
       document.querySelector('input').value = string;
     }
+    else if(e.target.innerHTML == 'DEL'){
+      string = string.slice(0, -1);
+      document.querySelector('input').value = string;
+    }
+
     else{
     console.log(e.target)
     string = string + e.target.innerHTML;
