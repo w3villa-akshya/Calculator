95442274b75786c423427ea2623d81efded6e04a
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-10-18 15:42:26 +0530

    added try catch

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
+      try{
+        string = eval(string);
+        document.querySelector('input').value = string;
+      }
+      catch{
+        document.querySelector('input').value = 'Error!';
+      }
     }
     else if(e.target.innerHTML == 'C'){
       string = ""
